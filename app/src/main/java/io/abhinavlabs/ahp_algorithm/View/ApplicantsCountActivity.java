42f209eea1d8effbfddc6d19417efa274719879c
Abhinav Das <abhinav.adtechs@gmail.com>
2017-10-28 15:44:36 +0530

    First page setup and layout done

---
+++ app/src/main/java/io/abhinavlabs/ahp_algorithm/View/ApplicantsCountActivity.java
@@ -0,0 +1,33 @@
+package io.abhinavlabs.ahp_algorithm.View;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import io.abhinavlabs.ahp_algorithm.R;
+
+public class ApplicantsCountActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_applicants_count);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/io/abhinavlabs/ahp_algorithm/View/MainActivity.java
+++ app/src/main/java/io/abhinavlabs/ahp_algorithm/View/MainActivity.java
@@ -1,9 +1,12 @@
 package io.abhinavlabs.ahp_algorithm.View;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +18,11 @@ public class MainActivity extends BaseActivity{
     @BindView(R.id.activity_main_toolbar)
     Toolbar toolbarMain ;
 
+    @BindView(R.id.activity_main_btn_next)
+    Button btnNext ;
+
+    MainActivity mainActivity ;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,16 @@ public class MainActivity extends BaseActivity{
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this) ;
+        mainActivity = this ;
 
         setSupportActionBar(toolbarMain);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainActivity, ApplicantsCountActivity.class) ;
+                startActivity(intent);
+            }
+        });
     }
 }
